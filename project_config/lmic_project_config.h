e2311cf92c6fb613e5ad3a73bb72018f3d999f17
Uren Thummar <urenthummar@gmail.com>
2020-09-28 22:04:23 +0530

    project config file updated to work in indian frequeny band

--- project_config/lmic_project_config.h
+++ project_config/lmic_project_config.h
@@ -1,10 +1,15 @@
 // project-specific definitions
 //#define CFG_eu868 1
-#define CFG_us915 1
+//#define CFG_us915 1
 //#define CFG_au915 1
 //#define CFG_as923 1
 // #define LMIC_COUNTRY_CODE LMIC_COUNTRY_CODE_JP	/* for as923-JP */
 //#define CFG_kr920 1
-//#define CFG_in866 1
+#define CFG_in866 1
 #define CFG_sx1276_radio 1
 //#define LMIC_USE_INTERRUPTS
+#define DISABLE_PING
+#define DISABLE_BEACONS
+#define LMIC_DEBUG_LEVEL 0
+#define USE_IDEETRON_AES
+//#define LMIC_ENABLE_DeviceTimeReq 1
